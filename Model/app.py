71107a7b9f8ff2dc6bc3924d354cb3b3dcb8fb58
CorinaPalade <corina203@yahoo.com>
2018-06-28 13:44:01 +0100

    deleted incorrect call to function

--- Model/app.py
+++ Model/app.py
@@ -4,23 +4,8 @@ from flask import render_template
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-#
-# def add_posts():
-#     for i in range(100):
-#         p = Post(id = i,
-#                  timestamp = date.today(),
-#                  title = 'title ' + str(i),
-#                  description = 'description ' + str(i),
-#                  upvotes = gauss(1, 100, 2),
-#                  downvotes = gauss(1, 100, 2))
-#
-#         db.session.add(p)
-#
-#     db.session.commit()
-
 @app.route('/')
 @app.route('/index')
 def index():
-    add_posts()
     user = {'username': 'Test User'}
     return render_template('index.html', title='Home', user=user)
