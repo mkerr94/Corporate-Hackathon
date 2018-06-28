5d8cbc57ee636e3cbff6507be8066f1c2cbb1c0c
CorinaPalade <corina203@yahoo.com>
2018-06-28 12:40:06 +0100

    created the database

--- Model/post_class.py
+++ Model/post_class.py
@@ -1,10 +1,9 @@
-
-from Model.app import db
+from app import db
 
 class Post(db.Model):
     __tablename__ = 'post'
 
-    id_post = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     timestamp = db.Column(db.Date())
     title = db.Column(db.String(500))
     description = db.Column(db.String(1000), unique = True)
@@ -12,4 +11,5 @@ class Post(db.Model):
 
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<Post id = {}, timestamp = {}, title = {}, description = {}, votes_number={}>'\
+            .format(self.id, self.timestamp, self.title, self.description, self.votes_number)
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 @app.route('/')
 @app.route('/index')
