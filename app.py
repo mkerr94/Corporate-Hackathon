5ce89b9ef2bf0c2915b2dccf842cde9672cdd5fa
CorinaPalade <corina203@yahoo.com>
2018-06-28 10:15:18 +0100

    added index.html

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello, World!"
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Test User'}
+    return render_template('index.html', title='Home', user=user)
